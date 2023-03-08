cb51277d0b12e8101d827c6febdbcb228c6daa45
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-08 14:38:04 -0300

    Update configuration

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ netspeed_rx, "Wl: %sB/s  ",           "wlan0" },
+	{ netspeed_rx, "Wl: %sB/s  ",           "wlp6s0" },
 	{ run_command, "Vol: %4s | ",           "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
   // WIP: Add OSS get volume feature
 	{ cpu_perc, "[CPU: %s%%] ",           NULL },
